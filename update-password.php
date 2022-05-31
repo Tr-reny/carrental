8a451966ea0bf1f88763bd2a2b4f716cf0d56d54
Tr-reny <rennykipkoech@gmail.com>
2022-05-31 18:04:40 -0400

    commit(commit"main")

--- update-password.php
+++ update-password.php
@@ -3,6 +3,7 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
+
 if(strlen($_SESSION['login'])==0)
   { 
 header('location:index.php');
--- vehical-details.php
+++ vehical-details.php
@@ -3,6 +3,7 @@ session_start();
 include('includes/config.php');
 error_reporting(0);
 if(isset($_POST['submit']))
+
 {
 $fromdate=$_POST['fromdate'];
 $todate=$_POST['todate']; 
